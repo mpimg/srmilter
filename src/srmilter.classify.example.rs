be0250eba8888de0f020a9e50185d76ecc562d37
Donald Buczek <buczek@molgen.mpg.de>
2025-07-10 09:33:00 +0200

    srmilter.classify.example: Reformat

--- src/srmilter.classify.example.rs
+++ src/srmilter.classify.example.rs
@@ -1,18 +1,24 @@
-/* */ {
+{ /* bracket required */
 
-if regex_is_match!("Täääst", subject) {
-    return ClassifyResult::Quarantine;
-}
+    // file format subject to change!
 
-if msg
-    .header(HeaderName::Other(Borrowed("X-Mailru-Msgtype")))
-    .is_some()
-{
-    if from_address.ends_with("@iscb.org") || from_address.ends_with("@news.arraystar.com") {
-        return ClassifyResult::Accept;
-    } else {
+    dbg!(sender);
+    dbg!(recipients);
+    dbg!(subject);
+
+    if regex_is_match!("Täääst", subject) {
         return ClassifyResult::Quarantine;
     }
-}
 
-/* */ }
+    if msg
+        .header(HeaderName::Other(Borrowed("X-Mailru-Msgtype")))
+        .is_some()
+    {
+        if from_address.ends_with("@iscb.org") || from_address.ends_with("@news.arraystar.com") {
+            return ClassifyResult::Accept;
+        } else {
+            return ClassifyResult::Quarantine;
+        }
+    }
+
+} /* bracket required */
