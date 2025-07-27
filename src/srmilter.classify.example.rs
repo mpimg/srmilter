0896f9bf8b7823013a6c0fb0e19c778be7c17f78
Donald Buczek <buczek@molgen.mpg.de>
2025-07-27 17:45:34 +0200

    srmilter.classify.example.rs: Update comments

--- src/srmilter.classify.example.rs
+++ src/srmilter.classify.example.rs
@@ -1,8 +1,31 @@
 { /* bracket required */
 
-    // file format subject to change!
+/*
+    file format subject to change!
 
+    available data:
 
+      id                 postfix queue ident or "test" or ""
+
+      sender             envelope sender (if known) or ""
+      recipients         envelope recipients ( &Vec<&String> )
+
+      from_address       address part of first or only From Address or ""
+      subject            Subject or ""
+      text               message text (text/plain alternative or converted text/html)
+
+      msg                &mail_parser::Message
+
+    available macros:
+
+      log!(...)          as println! but queue id automatically prefixed
+
+      quarantine!(...)   log and return with Quarantine status. Log defaults to file and line number
+      accept!(...)       log and return with Accept status. Log defaults to file and line number
+      reject!(...)       log and return with Reject. Log defaults to file and line number
+
+      regex_is_match!    from lazy_regex
+*/
 
     if regex_is_match!("Täääst", subject) {
         quarantine!();
