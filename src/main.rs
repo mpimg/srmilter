afb0abbb8b95f9e9653ab233e9e024c3c5a6d4aa
Donald Buczek <buczek@molgen.mpg.de>
2025-12-12 15:26:36 +0100

    Use std::process::Termination
    In the process of minimizing main, remove the extra code to display
    errors, just return the Result value from main. We accept that the
    runtime by default will display errors with the Debug trait and the
    error output becomes a little bit more verbose.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
 #![allow(unused_macros)]
-
-use srmilter::cli::xmain;
-use std::process::ExitCode;
-
 mod classify;
 
-fn main() -> ExitCode {
-    match xmain(classify::classify) {
-        Ok(_) => ExitCode::SUCCESS,
-        Err(e) => {
-            eprintln!("error: {e}");
-            ExitCode::FAILURE
-        }
-    }
+fn main() -> impl std::process::Termination {
+    srmilter::cli::xmain(classify::classify)
 }
