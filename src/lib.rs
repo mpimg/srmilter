748248d9621f61fc42f71821e7c83448744de02a
Donald Buczek <buczek@molgen.mpg.de>
2026-01-04 16:34:31 +0100

    Move three test functions into library crate
    Three test functions use interfaces which are not really
    required to be public. These are:
    
    - struct MaiInfoStorage and its fields
    - the fields of struct MailInfo
    
    Move the test functions into the library crate so that they run as unit
    test and not as integration test and can use internal interfaces.  This
    allows us to remove the public API in a following commit.

--- src/lib.rs
+++ src/lib.rs
@@ -381,3 +381,101 @@ pub fn array_contains(haystack: &[String], needle: &str) -> bool {
     }
     false
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_001() {
+        let storage = MailInfoStorage {
+            mail_buffer: std::fs::read("tests/parse_001.eml").unwrap(),
+            sender: "sender".to_string(),
+            recipients: vec!["recipient".to_string()],
+            id: "test".to_string(),
+            ..Default::default()
+        };
+
+        let mail_info = MailInfo {
+            storage: &storage,
+            msg: MessageParser::default()
+                .parse(&storage.mail_buffer)
+                .unwrap(),
+        };
+
+        assert_eq!(mail_info.get_sender(), "sender");
+        assert_eq!(mail_info.get_only_recipient(), "recipient");
+        assert_eq!(mail_info.get_from_address(), "donald.buczek@gmail.com");
+        assert_eq!(mail_info.get_from_name(), "Donald Buczek");
+        assert_eq!(mail_info.get_header_sender_address(), "");
+        assert_eq!(mail_info.get_spam_score(), 0f32);
+        assert_eq!(mail_info.get_to_address(), "emil.erpel@entenhausen.org");
+        assert_eq!(mail_info.get_to_name(), "Emil Erpel");
+        assert_eq!(
+            mail_info.get_subject(),
+            "Test mit einer relativ langen Header-Zeile, die hoffentlich zum Wrapping führt und dann auch noch mit Umlauten und Emoji 😀"
+        );
+        assert_eq!(mail_info.get_text(), "😘\r\n");
+        assert_eq!(
+            mail_info.get_remote_name(".mx.srv.dfn.de"),
+            "mail-lj1-f170.google.com"
+        );
+        let (name, ip, iprev) = mail_info.get_remote(".mx.srv.dfn.de");
+        assert_eq!(name, "mail-lj1-f170.google.com");
+        assert_eq!(ip, "209.85.208.170");
+        assert_eq!(iprev, "mail-lj1-f170.google.com");
+        let (name, ip, iprev) = mail_info.get_remote(".junk");
+        assert_eq!(name, "");
+        assert_eq!(ip, "");
+        assert_eq!(iprev, "");
+    }
+
+    #[test]
+    fn parse_002() {
+        let storage = MailInfoStorage {
+            mail_buffer: std::fs::read("tests/parse_002.eml").unwrap(),
+            sender: "sender".to_string(),
+            recipients: vec!["recipients".to_string()],
+            id: "test".to_string(),
+            ..Default::default()
+        };
+        let mail_info = MailInfo {
+            storage: &storage,
+            msg: MessageParser::default()
+                .parse(&storage.mail_buffer)
+                .unwrap(),
+        };
+        assert_eq!(
+            mail_info.get_subject(),
+            "New privacy policy at codeberg.org"
+        );
+    }
+
+    #[test]
+    fn test_only_recipients() {
+        let mut storage = MailInfoStorage::default();
+        {
+            let mail_info = MailInfo {
+                storage: &storage,
+                msg: mail_parser::Message::default(),
+            };
+            assert_eq!(mail_info.get_only_recipient(), "");
+        }
+        storage.recipients.push("foobar1".to_string());
+        {
+            let mail_info = MailInfo {
+                storage: &storage,
+                msg: mail_parser::Message::default(),
+            };
+            assert_eq!(mail_info.get_only_recipient(), "foobar1");
+        }
+        storage.recipients.push("foobar2".to_string());
+        {
+            let mail_info = MailInfo {
+                storage: &storage,
+                msg: mail_parser::Message::default(),
+            };
+            assert_eq!(mail_info.get_only_recipient(), "");
+        }
+    }
+}
